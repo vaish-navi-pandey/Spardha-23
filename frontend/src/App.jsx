2f853b46d45e418eed0254734f8e248d2ef2e38f
atulyadav745 <106299466+atulyadav745@users.noreply.github.com>
2023-10-03 00:16:51 +0530

    updated-logout button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
 	return (
 		<AuthProvider>
 			<BrowserRouter>
-      {/* <Navbar></Navbar> */}
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
